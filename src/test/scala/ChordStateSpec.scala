bc36de0ab2ff3719276921960bd4ef9bcf6dec16
Momijikawa <windy.imlovinit@gmail.com>
2013-12-21 23:58:24 +0900

    テスト改修

--- src/test/scala/ChordStateSpec.scala
+++ src/test/scala/ChordStateSpec.scala
@@ -7,8 +7,10 @@ import akka.actor.ActorSystem
 import akka.actor.ActorDSL._
 import akka.agent.Agent
 import scala.collection.immutable.HashMap
+import scala.concurrent.Await
+import org.specs2.time.NoTimeConversions
 
-class ChordStateSpec extends Specification with BeforeExample {
+class ChordStateSpec extends Specification with BeforeExample with NoTimeConversions {
   sequential
 
   import scala.concurrent.ExecutionContext.Implicits.global
@@ -46,9 +48,14 @@ class ChordStateSpec extends Specification with BeforeExample {
 
   "dataput" should {
     "put and get correctly" in {
+      import scala.concurrent.stm._
+      import scala.concurrent.duration._
+      assume(Stream[Byte](1, 2, 3) == Stream[Byte](1, 2, 3))
       val key = Seq[Byte](4, 5, 4, 5, 0, 7, 2, 1, 6, 7, 4, 1, 7, 1, 2, 3, 4, 5, 6, 7)
       val data = KVSValue(Stream[Byte](1, 2, 3, 4, 5))
-      ChordState.putDataToNode(key, data, state);
+      ChordState.putDataToNode(key, data, state)
+      Await.result(state.future(), 1 second)
+      println(state().dataholder)
       state().dataholder.get(key) must_== Some(KVSValue(Stream[Byte](1, 2, 3, 4, 5)))
     }
   }
